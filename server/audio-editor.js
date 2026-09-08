e9100a5c93c8aedf8fade166abf64953ec06cc7e
wongagung <ta.97156874sda2@gmail.com>
2026-09-08 23:29:45 +0700

    fix: convert audio editor helper to ESM

--- server/audio-editor.js
+++ server/audio-editor.js
@@ -1,19 +1,19 @@
+import { execFile } from "node:child_process";
+import { promisify } from "node:util";
 
-const {execFile}=require("child_process");
-const {promisify}=require("util");
-const run=promisify(execFile);
+const run = promisify(execFile);
 
-async function processAudio(input, output, opt={}){
- const filters=[];
- if(opt.gain) filters.push(`volume=${opt.gain}dB`);
- if(opt.fadeIn) filters.push(`afade=t=in:d=${opt.fadeIn}`);
- if(opt.fadeOut) filters.push(`afade=t=out:d=${opt.fadeOut}`);
- if(opt.compressor) filters.push("acompressor");
- if(opt.limiter) filters.push("alimiter");
- let args=["-y","-i",input];
- if(filters.length) args.push("-af",filters.join(","));
- args.push(output);
- await run("ffmpeg",args);
- return output;
+export async function processAudio(input, output, opt = {}) {
+  const filters = [];
+  if (opt.gain) filters.push(`volume=${opt.gain}dB`);
+  if (opt.fadeIn) filters.push(`afade=t=in:d=${opt.fadeIn}`);
+  if (opt.fadeOut) filters.push(`afade=t=out:d=${opt.fadeOut}`);
+  if (opt.compressor) filters.push("acompressor");
+  if (opt.limiter) filters.push("alimiter");
+
+  const args = ["-y", "-i", input];
+  if (filters.length) args.push("-af", filters.join(","));
+  args.push(output);
+  await run("ffmpeg", args);
+  return output;
 }
-module.exports={processAudio};
