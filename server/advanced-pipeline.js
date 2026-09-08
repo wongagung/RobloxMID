9e3faf16d53db5276381cc0a357be2e0a3bb6be7
wongagung <ta.97156874sda2@gmail.com>
2026-09-08 23:29:50 +0700

    fix: convert advanced pipeline helper to ESM

--- server/advanced-pipeline.js
+++ server/advanced-pipeline.js
@@ -1,25 +1,23 @@
+import { execFile } from "node:child_process";
+import { promisify } from "node:util";
+import crypto from "node:crypto";
+import path from "node:path";
 
-const {execFile}=require("child_process");
-const {promisify}=require("util");
-const crypto=require("crypto");
-const path=require("path");
-const exec=promisify(execFile);
+const exec = promisify(execFile);
 
-async function processAudio(input,opt={}){
- const output=path.join("uploads",crypto.randomUUID()+".mp3");
- let filters=[];
- if(opt.gain) filters.push(`volume=${opt.gain}dB`);
- if(opt.fadeIn) filters.push(`afade=t=in:d=${opt.fadeIn}`);
- if(opt.fadeOut) filters.push(`afade=t=out:d=${opt.fadeOut}`);
- if(opt.compressor) filters.push("acompressor");
- if(opt.limiter) filters.push("alimiter");
+export async function processAudio(input, opt = {}) {
+  const output = path.join("uploads", `${crypto.randomUUID()}.mp3`);
+  const filters = [];
+  if (opt.gain) filters.push(`volume=${opt.gain}dB`);
+  if (opt.fadeIn) filters.push(`afade=t=in:d=${opt.fadeIn}`);
+  if (opt.fadeOut) filters.push(`afade=t=out:d=${opt.fadeOut}`);
+  if (opt.compressor) filters.push("acompressor");
+  if (opt.limiter) filters.push("alimiter");
 
- let args=["-y","-i",input];
- if(filters.length) args.push("-af",filters.join(","));
- args.push(output);
+  const args = ["-y", "-i", input];
+  if (filters.length) args.push("-af", filters.join(","));
+  args.push(output);
 
- await exec("ffmpeg",args);
- return output;
+  await exec("ffmpeg", args);
+  return output;
 }
-
-module.exports={processAudio};
